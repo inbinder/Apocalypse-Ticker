9731b2328cb642a063a46b80729cd9ff827469dd
Zachary Manning <zachary.manning@gmail.com>
2022-05-21 13:41:21 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import render_template
+from flask import escape, request
+from pydantic import BaseModel, validator
+
+class StockModel(BaseModel):
+    """Class for parsing new stock data from a form."""
+    stock_symbol: str
+    number_of_shares: int
+    purchase_price: float
+
+    @validator('stock_symbol')
+    def stock_symbol_check(cls, value):
+        if not value.isalpha() or len(value) > 5:
+            raise ValueError('Stock symbol must be 1-5 characters')
+        return value.upper()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/about')
+def about():
+    #return render_template('about.html', company_name='TestDriven.io') 
+    return render_template('about.html')
+
+@app.route('/add_stock', methods=['GET', 'POST'])
+def add_stock():
+    if request.method == 'POST':
+    # Print the form data to the console
+      for key, value in request.form.items():
+          print(f'{key}: {value}')
+          
+    return render_template('add_stock.html')
+
+@app.route('/stocks/')
+def stocks():
+    return '<h2>Stock list ...</h2>'
+
+@app.route('/hello/<message>')
+def hello_message(message):
+    return f'<h1>Welcome {escape(message)}!</h1>'
+
+@app.route('/blog_posts/<int:post_id>')
+def display_blog_post(post_id):
+    return f'<h1>Blog Post #{post_id}...</h1>'
